f7fdb96b3ecc68073b8df001a8737b743a8015a5
yi.xuan <yihsuan717@gmail.com>
2024-05-15 01:32:52 +0800

    feat: new coming soon

--- pages/coming-soon.vue
+++ pages/coming-soon.vue
@@ -9,55 +9,56 @@ useSeoMeta({
 
 <!-- Home -->
 <template>
-  <dev class="flex flex-col min-h-screen bg-[#FDFCF8]">
-    <header class="flex items-center justify-center py-3"><Logo /></header>
+  <dev class="relative flex flex-col min-h-screen bg-[#FDFCF8] overflow-hidden">
+    <div>
+      <NuxtImg class="absolute -top-[3px] -left-[35px]" src="/images/soon/almond_tuiles.png" />
+      <NuxtImg class="absolute top-[6.2%] -right-[29px] -rotate-[22deg]" src="/images/soon/cupcake.png" />
+      <NuxtImg width="40" height="40" class="absolute top-[14.5%] -left-[7px]" src="/images/soon/c_1.png" />
+      <NuxtImg width="40" height="40" class="absolute top-[29%] -right-[19px]" src="/images/soon/c_1.png" />
+      <NuxtImg class="absolute top-[31.8%] -left-[37px] rotate-[46.55deg]" src="/images/soon/madeleine.png" />
+      <NuxtImg class="absolute top-[41.2%] -right-[19px]" src="/images/soon/almond_tuiles.png" />
+      <NuxtImg class="absolute top-[52.5%] -left-[12px]" src="/images/soon/c_2.png" />
+      <NuxtImg class="absolute bottom-[27.9%] -right-[17px] -rotate-[30.06deg]" src="/images/soon/madeleine.png" />
+      <NuxtImg class="absolute bottom-[32.2%] right-[70px]" src="/images/soon/c_3.png" />
+      <NuxtImg class="absolute bottom-[21%] -left-[4.9px] -rotate-[22deg]" src="/images/soon/cupcake.png" />
+    </div>
+    <header class="flex items-center justify-center pt-8"><Logo /></header>
     <main class="relative flex-1 flex flex-col">
       <div
-        class="z-10 relative grow pt-14 text-black text-5xl font-bold leading-snug text-center"
+        class="grow pt-14 text-center"
       >
         <NuxtImg
-          class="absolute top-10 left-0 w-14"
-          src="/images/soon/border_1.png"
-        />
-        <h1>即將推出</h1>
-        <h1>COMING</h1>
-        <h1>SOON !</h1>
-        <NuxtImg
-          class="-z-[1] absolute inset-0 mx-auto breathing w-10/12 sm:w-[300px]"
-          src="/images/soon/ellipse.png"
+          class="mx-auto w-[55%] sm:w-[360px] floating"
+          src="/images/soon/coming_soon.svg"
         />
         <NuxtImg
-          class="absolute top-1/2 right-0 w-13"
-          src="/images/soon/border_2.png"
+          class="mx-auto mt-4 sm:h-[36px]"
+          src="/images/soon/coming_soon_zh.png"
         />
       </div>
       <div class="relative flex flex-col items-center px-4 pt-12 py-5">
-        <span class="text-slate-800 text-3xl text-center leading-snug">
-          追蹤我們的社交平台，掌握最新資訊。
-        </span>
-        <div class="container flex justify-center py-3">
-          <div class="w-8 border-2 border-t border-primary-20 rounded-full" />
-        </div>
-        <NuxtImg
-          class="absolute -top-3 left-0 w-16"
-          src="/images/soon/border_3.png"
-        />
-        <div class="flex gap-4 mt-3">
+        <p class="text-[#867156] text-xl text-center leading-snug sm:text-2xl sm:flex">
+          追蹤我們的社交平台
+          <p>
+            掌握最新資訊
+          </p>
+        </p>
+        <div class="flex gap-5 mt-8">
           <NuxtLink
             to="https://www.instagram.com/msching_2022?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
             target="_blank"
-            ><NuxtImg src="/images/footer/icon_ig.png"
+            ><NuxtImg class="sm:h-[50px]" src="/images/footer/icon_ig.png"
           /></NuxtLink>
           <NuxtLink
             to="https://liff.line.me/1645278921-kWRPP32q/?accountId=984gfwdr"
             target="_blank"
-            ><NuxtImg src="/images/footer/icon_line.png"
+            ><NuxtImg class="sm:h-[50px]" src="/images/footer/icon_line.png"
           /></NuxtLink>
         </div>
       </div>
     </main>
     <footer>
-      <p class="p-6 text-xs text-center font-normal text-gray-500">
+      <p class="pb-10 text-xs text-center font-normal text-black">
         Copyright © 2024 msching.com 保留所有權利
       </p>
     </footer>
@@ -65,7 +66,6 @@ useSeoMeta({
 </template>
 
 <style scoped>
-@import url(https://fonts.googleapis.com/earlyaccess/cwtexhei.css);
 @keyframes breathing {
   0% {
     -webkit-transform: scale(0.9);
@@ -92,12 +92,19 @@ useSeoMeta({
   }
 }
 
-main {
-  font-family: 'cwTeXYen', sans-serif;
+@keyframes float {
+	0% {
+		transform: translatey(0px);
+	}
+	50% {
+		transform: translatey(-20px);
+	}
+	100% {
+		transform: translatey(0px);
+	}
 }
 
-.breathing {
-  -webkit-animation: breathing 5s infinite;
-  animation: breathing 5s infinite;
+.floating {
+	animation: float 6s ease-in-out infinite;
 }
 </style>
