aa2bc4965c330c7eff6d29915d50db13c89123fe
yi.xuan <yihsuan717@gmail.com>
2024-06-17 17:03:30 +0800

    feat: sidebar & fix compile error
    update
    
    update
    
    update

--- components/general/MainMenu.vue
+++ components/general/MainMenu.vue
@@ -1,16 +1,35 @@
 <script setup lang="ts">
-
+const menus = [
+  {
+    name: '首頁',
+    to: '/',
+  },
+  {
+    name: '最新消息',
+    to: '/',
+  },
+  {
+    name: '線上訂購',
+    to: '/',
+  },
+  {
+    name: '訂購QA/條款',
+    to: '/',
+  },
+  {
+    name: '聯繫我們',
+    to: '/',
+  }
+]
 </script>
 
 <template>
   <nav>
-    <NuxtLink to="/">首頁</NuxtLink>
-    <!-- <NuxtLink to="/products">最新消息</NuxtLink> -->
-    <!-- <NuxtLink to="/categories">最新消息</NuxtLink> -->
-    <!-- <NuxtLink to="/contact">線上訂購</NuxtLink> -->
-    <!-- <NuxtLink to="/contact">訂購QA/條款</NuxtLink> -->
-    <!-- <NuxtLink to="/contact">聯繫我們</NuxtLink> -->
-    <!-- <NuxtLink class="lg:hidden" to="/wishlist" :prefetch="false">Wishlist</NuxtLink> -->
-    <!-- <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink> -->
+    <template v-for="(menu, index) in menus" :key="index">
+      <UDivider v-if="index < menus.length" size="xs" class="py-0" :ui="{ border: { base: 'border-[#B8B8B8]/20'} }" />
+      <NuxtLink :to="menu.to" class="h-[72px] pl-6 flex items-center">
+        {{ menu.name }}
+      </NuxtLink>
+    </template>
   </nav>
 </template>
--- components/general/MobileMenu.vue
+++ components/general/MobileMenu.vue
@@ -4,9 +4,10 @@ const { toggleMobileMenu, appVersionInfo } = useHelpers();
 
 <template>
   <div class="bg-white flex flex-col max-w-lg shadow-lg top-0 bottom-0 left-0 w-9/12 z-50 fixed overflow-x-hidden">
-    <CloseIcon class="bg-white rounded-xl shadow-xl p-1.5" @click="toggleMobileMenu(false)" />
-    <!-- <div class="mt-8 text-center">{{ $t('messages.general.menu') }}</div> -->
-    <MainMenu class="m-4 grid p-4 text-gray-500 gap-6" />
+    <div class="w-full h-[130px]">
+      <!-- <CloseIcon class="bg-white rounded-xl shadow-xl p-1.5" @click="toggleMobileMenu(false)" /> -->
+    </div>
+    <MainMenu class="flex flex-col text-gray-500" />
     <div class="mt-auto text-center p-8 text-[10px] text-gray-400">
       <a href="/" :title="appVersionInfo">ms.ching v{{ appVersionInfo }}</a>
     </div>
