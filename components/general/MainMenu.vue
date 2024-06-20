ed32a7352d11d86b083b916ff6939e98b4a65df7
yi.xuan <yihsuan717@gmail.com>
2024-06-21 06:10:50 +0800

    feat: mobile sidebar menu

--- components/general/MainMenu.vue
+++ components/general/MainMenu.vue
@@ -1,35 +1,177 @@
 <script setup lang="ts">
 const menus = [
   {
-    name: '首頁',
+    id: 'home',
+    label: '首頁',
     to: '/',
   },
   {
-    name: '最新消息',
+    id: 'news',
+    label: '最新消息',
     to: '/',
   },
   {
-    name: '線上訂購',
+    id: 'onlineOrder',
+    label: '線上訂購',
     to: '/',
+    children: [
+      {
+        id: 'hotItems',
+        icon: 'heroicons-outline:fire',
+        label: '熱賣中',
+        to: '/',
+      },
+      {
+        id: 'cookies',
+        icon: 'heroicons-outline:lifebuoy',
+        label: '餅乾',
+        to: '/',
+      },
+      {
+        id: 'madeleines',
+        icon: 'heroicons-outline:cake',
+        label: '瑪德蓮',
+        to: '/',
+      },
+      {
+        id: 'festiveGifts',
+        icon: 'heroicons-outline:archive',
+        label: '節慶禮盒',
+        to: '/',
+      },
+    ],
   },
   {
-    name: '訂購QA/條款',
+    id: 'orderHelp',
+    label: '訂購QA/條款',
     to: '/',
+    children: [
+      {
+        id: 'orderQa',
+        icon: 'heroicons-outline:clipboard-document-list',
+        label: '訂購Q&A',
+        to: '/',
+      },
+      {
+        id: 'purchaseTerms',
+        icon: 'heroicons-outline:shopping-bag',
+        label: '購買條款',
+        to: '/',
+      },
+    ],
   },
   {
-    name: '聯繫我們',
+    id: 'aboutUs',
+    label: '關於我們',
     to: '/',
-  }
+  },
 ]
+
+const openMenus = ref<number[]>([])
+const selectedId = ref({ parent: '', child: '' })
+
+const toggleMenu = (index: number) => {
+  if (openMenus.value.includes(index)) {
+    openMenus.value = openMenus.value.filter((i) => i !== index)
+  } else {
+    openMenus.value.push(index)
+  }
+}
+
+const isMenuOpen = (index: number) => {
+  return openMenus.value.includes(index)
+}
+
+function onEnter(_el: Element, done: () => void) {
+  const el = _el as HTMLElement
+  el.style.height = '0'
+  el.offsetHeight // Trigger a reflow, flushing the CSS changes
+  el.style.height = el.scrollHeight + 'px'
+
+  el.addEventListener('transitionend', done, { once: true })
+}
+
+function onBeforeLeave(_el: Element) {
+  const el = _el as HTMLElement
+  el.style.height = el.scrollHeight + 'px'
+  el.offsetHeight // Trigger a reflow, flushing the CSS changes
+}
+
+function onAfterEnter(_el: Element) {
+  const el = _el as HTMLElement
+  el.style.height = 'auto'
+}
+
+function onLeave(_el: Element, done: () => void) {
+  const el = _el as HTMLElement
+  el.style.height = '0'
+
+  el.addEventListener('transitionend', done, { once: true })
+}
+
+const isSelected = (id: { parent?: string, child?: string }) => {
+  if (id.child) {
+    return selectedId.value.parent === id.parent && selectedId.value.child === id.child
+  } else {
+    return selectedId.value.parent === id.parent
+  }
+}
+
+const selectMenu = (id: { parent?: string, child?: string }) => {
+  id.parent && (selectedId.value.parent = id.parent)
+  id.child && (selectedId.value.child = id.child)
+}
+
 </script>
 
 <template>
   <nav>
-    <template v-for="(menu, index) in menus" :key="index">
-      <UDivider v-if="index < menus.length" size="xs" class="py-0" :ui="{ border: { base: 'border-[#B8B8B8]/20'} }" />
-      <NuxtLink :to="menu.to" class="h-[72px] pl-6 flex items-center">
-        {{ menu.name }}
-      </NuxtLink>
-    </template>
+    <ul>
+      <li v-for="(menu, index) in menus" :key="menu.id">
+        <UDivider
+          v-if="index < menus.length"
+          size="xs"
+          class="py-0"
+          :ui="{ border: { base: 'border-[#B8B8B8]/20' } }"
+        />
+        <div
+          @click="toggleMenu(index)"
+          @click.stop="selectMenu({ parent: menu.id })"
+          :class="{ 'bg-[#BAA086]/10 text-[#BAA086] font-bold': isSelected({ parent: menu.id }) }"
+          class="flex items-center justify-between m-3 p-3 rounded-lg cursor-pointer"
+        >
+          <NuxtLink :to="menu.to" class="flex-1">{{ menu.label }}</NuxtLink>
+          <span
+            v-if="menu.children"
+            class="i-heroicons-chevron-down-20-solid ml-auto w-6 h-6 transform transition-transform duration-200 flex-shrink-0 -rotate-90 text-[#B99F85]"
+            :class="{ 'rotate-0': isMenuOpen(index) }"
+          ></span>
+        </div>
+        <transition
+          @enter="onEnter"
+          @after-enter="onAfterEnter"
+          @before-leave="onBeforeLeave"
+          @leave="onLeave"
+          enter-active-class="transition transition-all duration-200 ease-out"
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          leave-active-class="transition transition-all duration-300 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <ul
+            v-if="menu.children && isMenuOpen(index)"
+            class="pl-4 mt-2"
+          >
+            <li v-for="(child, i) in menu.children" :key="i" class="mb-1 mr-3">
+              <UIcon v-if="child.icon" :name="child.icon" />
+              <NuxtLink :to="child.to" class="block px-4 py-2 rounded-lg" :class="{ 'bg-[#BAA086]/10 text-[#BAA086] font-bold': isSelected({ parent: menu.id, child: child.id }) }" @click.stop="selectMenu({ parent: menu.id, child: child.id })">{{
+                child.label
+              }}</NuxtLink>
+            </li>
+          </ul>
+        </transition>
+      </li>
+    </ul>
   </nav>
 </template>
