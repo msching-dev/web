83a671aa407e14267b19752b5c8e05da17728959
yi.xuan <yihsuan717@gmail.com>
2024-06-22 09:39:26 +0800

    fix: sidebar menu overflow

--- components/general/MobileMenu.vue
+++ components/general/MobileMenu.vue
@@ -7,7 +7,7 @@ const { toggleMobileMenu, appVersionInfo } = useHelpers();
     <div class="relative w-full h-[130px]">
       <CloseIcon class="absolute right-[22px] bottom-[29px] shadow-sm" @click="toggleMobileMenu(false)" />
     </div>
-    <MainMenu class="flex flex-col text-gray-500" />
+    <MainMenu class="flex flex-col text-gray-500 overflow-y-auto" />
     <div class="mt-auto text-center p-8 text-[10px] text-gray-400">
       <a href="/" :title="appVersionInfo">ms.ching v{{ appVersionInfo }}</a>
     </div>
