3c94fab1034bff140907e5ab10989a13d8463d33
yi.xuan <yihsuan717@gmail.com>
2024-10-28 01:07:02 +0800

    fix: build error & home banner add click event

--- components/general/Banner.vue
+++ components/general/Banner.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 const { type } = defineProps<{ type: 'home' | 'login' }>()
+const router = useRouter()
 
 const bannerSrc = computed(() => {
   switch (type) {
@@ -11,11 +12,17 @@ const bannerSrc = computed(() => {
       return '/images/banner.png'
   }
 })
+
+const handleBannerClick = () => {
+  if (type === 'home') {
+    router.push({ path: '/', query: { category: 'madeleine' } })
+  }
+}
 </script>
 <template>
   <div class="relative mx-auto">
     <NuxtImg
-      class="object-cover w-full h-auto"
+      class="object-cover w-full h-auto cursor-pointer"
       :src="bannerSrc"
       width="786"
       height="438"
@@ -25,6 +32,7 @@ const bannerSrc = computed(() => {
       preload
       :placeholder="[786, 438]"
       placeholder-class="blur-xl"
+      @click="handleBannerClick"
     />
   </div>
 </template>
