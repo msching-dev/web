ea4382734cd165ce263b7d848330fb17cc830c5a
yi.xuan <yihsuan717@gmail.com>
2024-11-10 19:42:07 +0800

    chore: add nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image', '@vueuse/nuxt'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@vueuse/nuxt', '@nuxthub/core'],
   components: [
     {
       path: '~/components',
